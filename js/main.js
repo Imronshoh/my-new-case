2db1eca19acb23dd7670021928138aaa5d35cff1
Imronshoh <monikapi2022@gmail.com>
2023-12-05 15:40:55 +0500

    a

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var elInp = document.querySelector(".inp");
+var elList = document.querySelector(".list");
+var arr = [
+  "mercedes",
+  "BMW",
+  "AUDI",
+  "chevrolet",
+  "RAVON",
+  "bugatti",
+  "BENTLEY",
+];
+function myFn() {
+  for (var i = 0; i < arr.length; i++) {
+    if (elInp.value == "katta" && arr[i].toUpperCase() == arr[i]) {
+      var newLi = document.createElement("li");
+      newLi.textContent = arr[i];
+      elList.appendChild(newLi);
+    } else if (elInp.value == "kichik" && arr[i].toLowerCase() == arr[i]) {
+      var newLi = document.createElement("li");
+      newLi.textContent = arr[i];
+      elList.appendChild(newLi);
+    }
+  }
+}
