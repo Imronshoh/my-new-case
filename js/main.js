ef0249e16aaceafff15c65b73c2e2b1dd3097a28
Imronshoh <monikapi2022@gmail.com>
2023-12-07 13:15:36 +0500

    ok

--- js/main.js
+++ js/main.js
@@ -15,7 +15,15 @@ function myFn() {
       var newLi = document.createElement("li");
       newLi.textContent = arr[i];
       elList.appendChild(newLi);
+    } else if (elInp.value == "Katta" && arr[i].toUpperCase() == arr[i]) {
+      var newLi = document.createElement("li");
+      newLi.textContent = arr[i];
+      elList.appendChild(newLi);
     } else if (elInp.value == "kichik" && arr[i].toLowerCase() == arr[i]) {
+      var newLi = document.createElement("li");
+      newLi.textContent = arr[i];
+      elList.appendChild(newLi);
+    } else if (elInp.value == "Kichik" && arr[i].toLowerCase() == arr[i]) {
       var newLi = document.createElement("li");
       newLi.textContent = arr[i];
       elList.appendChild(newLi);
